3b76703965b85fcd6fb390874d50d0d92839ca6b
Ryno <ryno.pierce@gmail.com>
2017-07-06 20:54:43 +0200

    Added password hashing on user model

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,14 +1,27 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 10
-};
+var password = '123abc!';
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
-var decoded = jwt.verify(token, '123abc');
-console.log(`Decoded: ${JSON.stringify(decoded)}`);
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) =>{
+//     console.log(hash);
+//   });
+// });
+
+var hashedPassword = '$2a$10$rdPYMeGm6QKVX18dq.pQPOEXKkgEvyNCjFox0gRiV7VkqSP3fm8bK';
+bcrypt.compare(password, hashedPassword, (err, result) => {
+  console.log(result);
+});
+// var data = {
+//   id: 10
+// };
+//
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+// var decoded = jwt.verify(token, '123abc');
+// console.log(`Decoded: ${JSON.stringify(decoded)}`);
 // var message = 'I am user number 3';
 // var hash = SHA256(message).toString();
 //
